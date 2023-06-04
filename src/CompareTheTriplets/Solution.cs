892761f70a4ee6b03a2c0847d178a39eb27462eb
Dennis Poulton <dennis.poulton@honeywell.com>
2023-06-04 17:48:04 +0200

    CompareTheTriplets kata added.

---
+++ src/CompareTheTriplets/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace CompareTheTriplets;
+
+public class Solution
+{
+    public int[] CompareTriplets(int[] alice, int[] bob)
+    {
+        var aliceScore = 0;
+        var bobScore = 0;
+
+        for (var i = 0; i < alice.Length; i++)
+        {
+            var aliceRating = alice[i];
+            var bobRating = bob[i];
+
+            if (aliceRating > bobRating)
+            {
+                aliceScore++;
+            }
+            else if(aliceRating < bobRating)
+            {
+                bobScore++;
+            }
+        }
+
+        return new[] { aliceScore, bobScore };
+    }
+}
---
+++ src/CompareTheTriplets/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CompareTheTriplets
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        private Solution _sut = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new Solution();
+        }
+
+        [TestCase(new[] {5, 6, 7}, new[] {3, 6, 10}, new[] {1, 1})]
+        [TestCase(new[] {1, 2, 3}, new[] {3, 2, 1}, new[] {1, 1})]
+        [TestCase(new[] {17, 28, 30}, new[] {90, 16, 8}, new[] {2, 1})]
+        public void Given_Challenge_Points_For_Alice_And_Bob_Should_Find_Their_Comparison_Points(int[] alice, int[] bob, int[] expected)
+        {
+            // Arrange
+            // Act
+            var result = _sut.CompareTriplets(alice, bob);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
